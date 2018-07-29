524aef51f6c452b6902a681bc5de442264c4804e
Alvaro <acs.lmv@gmail.com>
2018-07-29 17:23:37 -0500

    AcsPref

--- library/src/main/java/acs/utils/Pref.java
+++ library/src/main/java/acs/utils/Pref.java
@@ -1,38 +1,37 @@
 package acs.utils;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
 
 import com.google.gson.Gson;
 
-public class Pref {
+@SuppressLint("CommitPrefEdits")
+abstract class AcsPref {
 
-    private static Pref instance;
+    public SharedPreferences sp;
+    public Editor editor;
 
-    private SharedPreferences sp;
-    private Editor editor;
-
-    // Keys
-    private static final String _NAME = "ACS_v2";
-
-    public Pref(Context context) {
-        this.sp = context.getSharedPreferences(_NAME, Context.MODE_PRIVATE);
+    public AcsPref(Context context, String pref_name) {
+        this.sp = context.getSharedPreferences(pref_name, Context.MODE_PRIVATE);
         this.editor = sp.edit();
-    }
-
-    public static Pref ins(Context ctx) {
-        if (instance == null)
-            instance = new Pref(ctx);
-        return instance;
     }
 
     public String getString(String key) {
         return sp.getString(key, "");
     }
 
+    public String getString(String key, String defaultValue) {
+        return sp.getString(key, defaultValue);
+    }
+
     public int getInt(String key) {
         return sp.getInt(key, 0);
+    }
+
+    public int getInt(String key, int defaultValue) {
+        return sp.getInt(key, defaultValue);
     }
 
     public long getLong(String key) {
